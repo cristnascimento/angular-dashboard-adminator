840222bf83997e9cdb01d186bcb6afc8db743bac
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-18 20:57:19 -0300

    Fixing search input. closes #30

--- dashboard/src/app/dashboard/dashboard.component.ts
+++ dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild("search1",  {static: false}) searchInput: ElementRef;
+  status: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickEvent(e): void {
+    e.preventDefault();
+    this.status = !this.status;
+    setTimeout(() => this.searchInput.nativeElement.focus());
   }
 
 }
