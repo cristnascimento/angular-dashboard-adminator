ac3f3ffa1afe3af42f3bfac3a7f54bc147c4f8b7
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-18 15:56:32 -0300

    Adding email component. closes #24

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule
