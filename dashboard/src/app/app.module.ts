39d45cb9c8ce996eb95e8dd789eefd64c310e63b
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-18 20:55:23 -0300

    Fixing dependencies in index.html. closes #28

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmailComponent } from './email/email.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderImportsComponent } from './header-imports/header-imports.component';
+import { FooterImportsComponent } from './footer-imports/footer-imports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    EmailComponent
+    EmailComponent,
+    HeaderImportsComponent,
+    FooterImportsComponent
   ],
   imports: [
     BrowserModule,
