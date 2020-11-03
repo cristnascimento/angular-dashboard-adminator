9e85d80d7653d457b55c503c2e5b215dfdea945d
Cristiano Nascimento <cristnascimento@gmail.com>
2020-11-02 21:48:53 -0300

    Adding Perfect Scrollbar from NPM. closes #47

--- dashboard/src/app/email/email.component.ts
+++ dashboard/src/app/email/email.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Inject, Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
   styleUrls: ['./email.component.scss']
 })
-export class EmailComponent implements OnInit {
+export class EmailComponent implements OnInit, AfterViewInit {
+  @ViewChild("search1",  {static: false}) searchInput: ElementRef;
+  status: boolean = false;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    //this.ngAfterViewInit();
   }
 
+  ngOnChanges(): void {
+    //this.ngAfterViewInit();
+  }
+
+  ngAfterViewInit(): void {
+    console.log("loading heyhey...");
+    //const ps = new PerfectScrollbar('#heyhey');
+
+    const scrollables =  this.elementRef.nativeElement.querySelectorAll('.scrollable');
+    if (scrollables.length > 0) {
+      console.log("LENGTH: " + scrollables.length);
+      scrollables.forEach( el => {
+        new PerfectScrollbar(el);
+      });
+    }
+  }
 }
+
