173cd4dd81106f51045e3015fe25ea6653faf857
Cristiano Nascimento <cristnascimento@gmail.com>
2020-11-06 21:42:03 -0300

    Adding calendar dependencies. closes #51

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmailComponent } from './email/email.component';
 import { ComposeComponent } from './compose/compose.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'email', component: EmailComponent },
   { path: 'compose', component: ComposeComponent },
+  { path: 'calendar', component: CalendarComponent },
   { path: '', component: DashboardComponent },
 ];
 
