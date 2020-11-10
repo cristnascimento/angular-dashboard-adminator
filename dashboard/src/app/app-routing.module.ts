c5c97b047ea5a7c5d6e4abcfddaf7e3076593ee4
Cristiano Nascimento <cristnascimento@gmail.com>
2020-11-09 22:14:26 -0300

    Adding chat component. closes #52

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmailComponent } from './email/email.component';
 import { ComposeComponent } from './compose/compose.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'email', component: EmailComponent },
   { path: 'compose', component: ComposeComponent },
   { path: 'calendar', component: CalendarComponent },
+  { path: 'chat', component: ChatComponent },
   { path: '', component: DashboardComponent },
 ];
 
