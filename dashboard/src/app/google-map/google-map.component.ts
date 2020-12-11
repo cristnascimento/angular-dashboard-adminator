14d30b5806eb5787558080a50db337e87c061267
Cristiano Nascimento <cristnascimento@gmail.com>
2020-12-10 21:52:26 -0300

    Init google-map. closes #103

--- dashboard/src/app/google-map/google-map.component.ts
+++ dashboard/src/app/google-map/google-map.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Inject, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {initGoogleMaps} from './initGoogleMaps';
 
 @Component({
   selector: 'app-google-map',
   templateUrl: './google-map.component.html',
   styleUrls: ['./google-map.component.scss']
 })
-export class GoogleMapComponent implements OnInit {
+export class GoogleMapComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef) { }
+  
+  //constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit(): void {
+    console.log("---- google maps-----");
+    
+    initGoogleMaps();
+    
+      let urls = [
+      {type:"text/javascript", src:"https://maps.googleapis.com/maps/api/js?key=AIzaSyDW8td30_gj6sGXjiMU0ALeMu1SDEwUnEA&callback=initMap"},
+      
+    ];
+  
+    urls.forEach( item => {
+      const s = this.document.createElement('script');
+      s.type = item.type;
+      s.src = item.src;
+      
+      if (item.src.includes("maps.googleapis")) {
+        s.defer = "defer";
+        console.log("adding defer")
+      }
+      this.elementRef.nativeElement.appendChild(s);
+      
+    });
+    }
 
 }
